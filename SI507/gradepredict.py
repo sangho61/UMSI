043201766607fcebc48c1cc8b4ef40c5346c5bca
sangho61 <sanghoeum@gmail.com>
2018-01-05 08:51:58 -0500

    made si507 folder and passignment 1

---
+++ SI507/gradepredict.py
@@ -0,0 +1,19 @@
+
+# compute scores for each category
+
+# drop lowerst scores for compute_homeworks
+# params: list of homework Grades
+# return: homework score
+def compute_homeworks():
+    pass
+
+# drop lowest scores for lectures
+def compute_lectures():
+    pass
+
+# drop lowest scores for discussion sections
+def compute_discussion():
+    pass
+
+def compute_other():
+    pass
