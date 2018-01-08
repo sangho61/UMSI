560d8c89180b5fb664896dc099c34f6042617814
sangho61 <sanghoeum@gmail.com>
2018-01-08 18:57:54 -0500

    tictactoe_draft

---
+++ SI507/tictactoe_draft.py
@@ -0,0 +1,41 @@
+# subproblem_1: Creating a board
+# parameters:
+# returns:
+Function
+    pass
+
+# subproblem_:1
+# parameters:
+# returns:
+Function
+    pass
+
+# subproblem_3: Update a board
+# parameters: Which location, whose turn, board
+# returns: board
+Function
+    pass
+
+# subproblem_:1
+# parameters:
+# returns:
+Function
+    pass
+
+# subproblem_:1
+# parameters:
+# returns:
+Function
+    pass
+
+# subproblem_:1
+# parameters:
+# returns:
+Function
+    pass
+# Players take turn
+# create board
+# update board
+# Test if a player has won
+# Test if it's a draw
+# check if it's a valid move
